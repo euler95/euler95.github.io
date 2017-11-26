d342775c1e8fac23753d6eca56ba310013e5c89f
euler95 <jasandov@ucm.es>
2017-11-26 21:29:55 +0100

    8000

--- practica3.js
+++ practica3.js
@@ -777,7 +777,7 @@ var game = function() {
 		stage.insert(new Q.Water({y:400,x:3050}));
 		stage.insert(new Q.Broken({y:400,x:3700}));
 		setTimeout(function(){stage.insert(new Q.Sobre2({y:300,x:4800}));},7000);
-		setTimeout(function(){stage.insert(new Q.Sobre2({y:300,x:4800}));},1000);
+		setTimeout(function(){stage.insert(new Q.Sobre2({y:300,x:5300}));},8000);
 		stage.insert(new Q.Pixar({y:400,x:8500}));
 		stage.insert(new Q.Sobre2({y:400,x:9500}));
 		stage.insert(new Q.Pizarra({y:400,x:10500}));
