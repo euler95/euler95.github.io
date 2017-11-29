4a135dd120a158f0c3f87f17bf0a4fd066056891
euler95 <jasandov@ucm.es>
2017-11-29 17:53:37 +0100

    triplazo

--- practica3.js
+++ practica3.js
@@ -886,7 +886,7 @@ var game = function() {
 		newHeight = Q.height / h;
 		
 		if(greater){
-			if(newWidth > newHeight) return newWidth;
+			if(newWidth > newHeight) return newHeight;
 			else return newHeight;
 		}else{
 			if(newWidth < newHeight) return newWidth;
